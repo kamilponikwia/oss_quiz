f33bbcf63645c4f55353bf0ec2c183ad733be3b0
kamil.ponikwia <kamil.ponikwia@gmail.com>
2021-12-28 11:08:01 +0300

    Answer for Question 1

---
+++ draw_shape.sh
@@ -0,0 +1,41 @@
+#! /bin/bash 
+
+read -p "Enter integer value: " value
+
+if [ $(( value % 2 )) != 0 ]
+then
+	for (( i=1; i<=$value; i++ ))
+	do
+		for (( j=$value; j>=i; j-- ))
+		do
+			echo -n " "
+		done
+		for (( k=1; k<=i; k++ ))
+		do
+			echo -n " *"
+			temp=`expr $temp + 1`
+		done
+		echo ""
+	done
+	
+	half=`expr $value - 1`
+	for (( i=$half; i>=1; i-- ))
+	do
+		for (( j=i; j<=$half; j++ ))
+		do
+			if [ $j == $half ]
+			then
+				echo -n " "
+			fi
+			echo -n " "
+		done
+		for (( k=1; k<=i; k++ ))
+		do
+			echo -n " *"
+			temp=`expr $temp + 1`
+		done
+		echo ""
+	done
+else
+	echo "Value not perfect."
+fi
